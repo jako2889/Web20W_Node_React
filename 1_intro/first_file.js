6ce5b5d3134697518773fdb1b8ccb8d2a1b63670
Jakob <jako2889@stud.kea.dk>
2020-08-26 14:18:38 +0200

    first basics done

---
+++ 1_intro/first_file.js
@@ -0,0 +1,20 @@
+// Exercise 1
+const firstName = "Anders";
+const lastName = "Latif";
+// EXERCISE
+// show in the console
+// My first name is Anders and my last name is Latif
+
+console.log("My first name " + firstName + " and my last name is " + lastName);
+console.log(`My first name ${firstName} and my last name is ${lastName}`);
+
+// Exercise 2 - Numbers and Strings
+
+const year = "2019";
+const number = 1;
+
+// Add the year plus the number
+// The result should be 2018
+// You cannot touch line 1 or 2
+
+console.log(parseInt(year) + 1);
